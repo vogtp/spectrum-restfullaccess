6a98de59d137044a08eedb863606fd5500820bec
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-03-26 12:19:25 +0200

    better error handling

--- src/ch/almana/spectrum/rest/net/HttpClientRequestHandler.java
+++ src/ch/almana/spectrum/rest/net/HttpClientRequestHandler.java
@@ -1,21 +1,16 @@
 package ch.almana.spectrum.rest.net;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -27,10 +22,19 @@ public class HttpClientRequestHandler implements IRequestHandler {
 
 	private static final String PATH_SEP = "/";
 	private final IRequestConfig settings;
+	private final DefaultHttpClient httpClient;
 
 	public HttpClientRequestHandler(IRequestConfig settings) {
 		super();
 		this.settings = settings;
+		final UsernamePasswordCredentials creds = new UsernamePasswordCredentials(settings.getUsername(), settings.getPassword());
+
+		httpClient = new DefaultHttpClient();
+
+		// post 8 : httpClient = AndroidHttpClient.newInstance("onetouch", context);
+
+		AuthScope authscope = AuthScope.ANY;
+		httpClient.getCredentialsProvider().setCredentials(authscope, creds);
 	}
 
 	public String getPayload(BaseModelAccess modelAccess) throws IOException {
@@ -40,45 +44,6 @@ public class HttpClientRequestHandler implements IRequestHandler {
 		path.append(modelAccess.getRestNoun());
 		URL url = new URL(settings.getSpectroServerProtocoll(), settings.getSpectroServerName(), path.toString());
 		return httpClientPost(url.toString(), modelAccess);
-	}
-
-	protected HttpURLConnection doGetRequest(BaseModelAccess modelAccess, StringBuilder path) throws MalformedURLException, IOException, ProtocolException {
-		int throttlesize = settings.getThrottlesize();
-		String attrSep = "?";
-		if (throttlesize > 0) {
-			path.append(attrSep);
-			path.append("throttlesize=").append(throttlesize);
-			attrSep = "&";
-		}
-		for (String attr : modelAccess.getAttributesHandles()) {
-			path.append(attrSep).append("attr=").append(attr);
-			attrSep = "&";
-		}
-
-		URL url = new URL(settings.getSpectroServerProtocoll(), settings.getSpectroServerName(), path.toString());
-		Logger.i("Loading " + url.toString());
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Accept", "application/json");
-
-		return conn;
-	}
-
-	protected HttpURLConnection doPostRequest(BaseModelAccess modelAccess, StringBuilder path) throws MalformedURLException, IOException,
-			ProtocolException {
-
-		URL url = new URL(settings.getSpectroServerProtocoll(), settings.getSpectroServerName(), path.toString());
-		Logger.i("Loading " + url.toString());
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		// conn.setRequestMethod("GET");
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-		conn.setRequestProperty("Accept", "application/json");
-
-		BufferedWriter postWriter = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-		postWriter.write(getPostConfig(modelAccess));
-		//		postWriter.write("");
-		return conn;
 	}
 
 	private String getPostConfig(BaseModelAccess modelAccess) {
@@ -92,7 +57,7 @@ public class HttpClientRequestHandler implements IRequestHandler {
 		}
 		sb.append(" xmlns:rs=\"http://www.ca.com/spectrum/restful/schema/request\"\n" +
 				"  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-				"  xsi:schemaLocation=\"http://www.ca.com/spectrum/restful/schema/request ../../../xsd/Request.xsd \">");
+				"  xsi:schemaLocation=\"http://www.ca.com/spectrum/restful/schema/request ../../../xsd/Request.xsd \">\n");
 		sb.append("\n");
 		for (String attr : modelAccess.getAttributesHandles()) {
 			sb.append("<rs:requested-attribute id=\"");
@@ -106,60 +71,50 @@ public class HttpClientRequestHandler implements IRequestHandler {
 		return sb.toString();
 	}
 
-	protected String httpClientPost(String url, BaseModelAccess modelAccess) {
-		final UsernamePasswordCredentials creds = new UsernamePasswordCredentials(settings.getUsername(), settings.getPassword());
+	protected String httpClientPost(String url, BaseModelAccess modelAccess) throws IOException {
 
 		HttpPost post = new HttpPost(url);
+		post.setHeader("Content-Type", "application/xml");
 		post.setHeader("Accept", "application/json");
-		//		get.setHeader("Content-type", "application/json");
 
 		try {
 			String postConfig = getPostConfig(modelAccess);
 			HttpEntity entity = new StringEntity(postConfig);
 			post.setEntity(entity);
 		} catch (UnsupportedEncodingException e) {
-			Logger.e("Cannot set post data", e);
+			Logger.e("Cannot set post data due to wrong encoding", e);
 		}
 
 		HttpResponse resp = null;
 		BufferedReader in = null;
 		try {
-			DefaultHttpClient httpClient = new DefaultHttpClient();
-			// post 8 : httpClient = AndroidHttpClient.newInstance("onetouch", context);
-
-			AuthScope authscope = AuthScope.ANY;
-			httpClient.getCredentialsProvider().setCredentials(authscope, creds);
 
 			resp = httpClient.execute(post);
 
-			if (resp != null) {
-				in = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
-				StringBuffer sb = new StringBuffer("");
-				String line = "";
-				String NL = System.getProperty("line.separator");
-				while ((line = in.readLine()) != null) {
-					sb.append(line + NL);
-				}
-				in.close();
-				String page = sb.toString();
-				System.out.println(page);
-				return sb.toString();
-			} else {
-				return "no data";
+			if (resp == null) {
+				throw new IOException("Got null repsonse from spectrum");
 			}
-		} catch (ClientProtocolException e) {
-			Logger.e("HTTP protocol error", e);
-			return e.getMessage();
-		} catch (IOException e) {
-			Logger.e("Communication error", e);
-			return e.getMessage();
+			StatusLine statusLine = resp.getStatusLine();
+			if (statusLine.getStatusCode() > 299) {
+				String string = "Spectrum status response: " + statusLine.toString();
+				Logger.w(string);
+				throw new IOException(string);
+			}
+
+			in = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			String page = sb.toString();
+			System.out.println(page);
+			return sb.toString();
 		} finally {
 			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				in.close();
 			}
 		}
 	}
