bf1c00af16b5806014e75a40413ab76771aafddb
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-08-31 14:31:46 +0200

    stuff for gwt

--- src/ch/almana/spectrum/rest/model/GenericModel.java
+++ src/ch/almana/spectrum/rest/model/GenericModel.java
@@ -1,9 +1,12 @@
 package ch.almana.spectrum.rest.model;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
-public class GenericModel {
+public class GenericModel implements Serializable {
 
+	private static final long serialVersionUID = -1018376056217832189L;
 	private final String id;
 	private final Map<String, String> attributes;
 
@@ -23,5 +26,26 @@ public class GenericModel {
 	
 	public Map<String, String> getAttributes() {
 		return attributes;
+	}
+
+	public Map<String, String> getAttributesNamed() {
+		Map<String, String> namedAttriubtes = new HashMap<String, String>(attributes.size());
+		for (String key : attributes.keySet()) {
+			String name = SpectrumAttibute.attributeNames.get(key);
+			name = name == null ? key : name;
+			namedAttriubtes.put(name, attributes.get(key));
+		}
+		return namedAttriubtes;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+
+		for (String key : attributes.keySet()) {
+			sb.append(key).append(" -> ").append(attributes.get(key)).append("; ");
+		}
+
+		return super.toString();
 	}
 }
